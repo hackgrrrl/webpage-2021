97f5468fc87f14d43a650603f21bbdce50143dff
naftalima <npl@cin.ufpe.br>
2021-02-28 17:55:26 -0300

    feat: add appbar and home

---
+++ src/components/Appbar.vue
@@ -0,0 +1,25 @@
+<template lang="pug">
+v-container
+  v-app-bar
+    v-img.shrink.mr-2(src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTKZOiUFIa5WBTcmlNeUzEXCr6AW9m0HBzIAQ&usqp=CAU' transition='scale-transition' width='40')
+    v-tabs(centered)
+      v-tab o que é
+      v-tab edições anteriores
+      v-tab quem faz
+      v-tab edição 2021
+      v-tab fale conosco
+</template>
+
+<script>
+  export default {
+    name: 'Appbar',
+
+    data: () => ({
+      //
+    }),
+  }
+</script>
+
+<style lang="sass" scoped>
+
+</style>
---
+++ src/components/Home.vue
@@ -0,0 +1,15 @@
+<template lang="pug">
+  v-container
+    
+</template>
+
+<script>
+  export default {
+    name: 'Home',
+
+    data: () => ({
+      //
+    }),
+  }
+</script>
+<style lang="sass" scoped> </style>
