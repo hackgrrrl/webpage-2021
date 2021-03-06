88601c4acdaef9d52b8f42015637f8debefa3b8a
Beatriz Alves <babs@cin.ufpe.br>
2021-03-06 12:44:29 -0300

    feature: change about component layout for mobile

--- src/components/About.vue
+++ src/components/About.vue
@@ -4,11 +4,13 @@ div.about
     v-col(cols='3' v-if='!isMobile')
       v-img.wave-img(src="../assets/pt2_1.png")
     v-col(style='text-align: center;')
+      v-img.xs-wave-img(v-if='isMobile' src='../assets/pt2_1.png')
       h1.topic o que é o Hack GRRRL?
       p 
         | Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
         | Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
         | when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+      v-img.xs-ball-img(v-if='isMobile' src='../assets/pt2_2.png')
       h1.topic nossa missão
       p 
         | Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
@@ -51,4 +53,16 @@ div.about
 p
   margin-top: 1vw
   color: #707070
+.xs-wave-img
+  position: relative
+  width: 10%
+  height: auto
+  top: 3vh
+  left: 5vw
+.xs-ball-img
+  position: relative
+  width: 7%
+  height: auto
+  top: 5vh
+  left: 92vw
 </style>
