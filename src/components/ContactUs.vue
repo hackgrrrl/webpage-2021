85a5ec245277a4fa146da3eeae6e8a61bac02b58
Beatriz Alves <babs@cin.ufpe.br>
2021-03-06 19:55:57 -0300

    fix: adjustments in contactUs component

--- src/components/ContactUs.vue
+++ src/components/ContactUs.vue
@@ -1,24 +1,25 @@
 <template lang="pug">
-div.about
+div.contact-us
   v-row(align="center")
-    v-col(cols='3' v-if='!isMobile')
-      v-img.wave-img(src="../assets/pt2_1.png")
+    v-col(cols='2' v-if='!isMobile')
+      v-img.triangle-img(src="../assets/pt6_1.png")
     v-col(style='text-align: center;')
       h1 fale conosco!
       p Tem alguma dúvida? Gostaria de nos patrocinar? Deixe aqui suas informações para entrarmos em contato assim que possível!
       v-row
         v-col
-          v-text-field(v-model="form.name" label="Nome" outlined color="#6FDDD5")
+          v-text-field.blue-border(v-model="form.name" label="Nome" solo flat hide-details color="#6FDDD5")
         v-col
-          v-text-field(v-model="form.email" label="Email" outlined color="#6FDDD5")
+          v-text-field.blue-border(v-model="form.email" label="Email" solo flat hide-details color="#6FDDD5")
         v-col
-          v-text-field(v-model="form.phone" label="Telefone (opcional)" outlined color="#6FDDD5")
+          v-text-field.blue-border(v-model="form.phone" label="Telefone (opcional)" solo flat hide-details color="#6FDDD5")
       v-row
-        v-textarea(v-model="form.description" label="Digite sua mensagem aqui..." outlined auto-grow rows="4" )
-      v-row
+        v-col(cols='12')
+          v-textarea.blue-border(v-model="form.description" label="Digite sua mensagem aqui..." solo flat auto-grow rows="4")
+      v-row.pink-btn(justify='center')
         v-btn(depressed color="#FF99CC") Enviar
-    v-col(cols='3' v-if='!isMobile')
-      v-img.ball-img(src="../assets/pt2_2.png")
+    v-col(cols='2' v-if='!isMobile')
+      v-img.wave-img(src="../assets/pt6_2.png")
 </template>
 
 <script>
@@ -41,28 +42,24 @@ div.about
   }
 </script>
 <style lang="sass" scoped>
-.wave-img
+.contact-us
   position: relative
-  width: 42%
+  margin-top: 2vw
+  margin-bottom: 1vw
+.triangle-img
+  position: absolute
+  width: 4.5%
   height: auto
   top: 1vw
-  left: 18.9%
-.ball-img
-  position: relative
-  width: 25%
+  left: 92%
+.wave-img
+  position: absolute
+  width: 6%
   height: auto
-  top: 10vh
-  left: 6.1vw
-.xs-wave-img
-  position: relative
-  width: 10%
-  height: auto
-  top: 3vh
-  left: 5vw
-.xs-ball-img
-  position: relative
-  width: 7%
-  height: auto
-  top: 5vh
-  left: 92vw
+  top: 38vh
+  left: 12%
+.blue-border
+  border: 3px solid #6FDDD5
+.pink-btn
+  margin-top: 3.5vh
 </style>
