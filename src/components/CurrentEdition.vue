9e537413eedd889fe3e18655dd105656c21a1e97
naftalima <npl@cin.ufpe.br>
2021-03-06 20:39:38 -0300

    feat: add ui components

--- src/components/CurrentEdition.vue
+++ src/components/CurrentEdition.vue
@@ -1,10 +1,22 @@
 <template lang="pug">
-div.home
-  v-img.ball-image(src='../assets/pt1_1.png')
-  v-img.triangle-image(src='../assets/pt1_2.png')
-  v-img.logo(src='../assets/titulo.png')
-  v-img.wave-image(src='../assets/pt1_3.png')
-  v-img.hill-image(src='../assets/pt1_4.png')
+div.current-edtion
+  v-row(align="center" justify="center")
+    v-col
+      v-img.ball-image(src='../assets/pt1_1.png')
+      v-img.triangle-image(src='../assets/pt1_2.png')
+    v-col
+      h2.text edição 2020
+      h1.text informações em breve!!
+      v-spacer
+      p.text Quer receber as novidades em primeira mão? Assine nossa newsletter!
+      v-row
+        v-col
+
+        v-col
+          v-btn(depressed color="#FF99CC") Enviar
+    v-col
+      v-img.wave-image(src='../assets/pt1_3.png')
+      v-img.hill-image(src='../assets/pt1_4.png')
 </template>
 
 <script>
@@ -17,7 +29,13 @@ div.home
   }
 </script>
 <style lang="sass" scoped>
-
+.current-edtion
+  width: 100%
+  position: relative
+  height: 38.4vw
+  background-color: #101111
+.text
+  color: #FFFFFF
 .ball-image
   position: absolute
   width: 8.7%
@@ -48,9 +66,5 @@ div.home
   height: auto
   right: 0%
   top: 18.2vw
-.home
-  width: 100%
-  position: relative
-  height: 38.4vw
-  background-color: #101111
+
 </style>
