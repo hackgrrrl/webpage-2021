bff3fc48dedb11c7e11d3b3fb8b4b520f1d7abe0
lfp2 <lfp2@cin.ufpe.br>
2021-03-21 00:04:55 -0300

    feat/initial version of send email

--- src/components/ContactUs.vue
+++ src/components/ContactUs.vue
@@ -1,45 +1,88 @@
 <template lang="pug">
 v-container#contact-us(fluid)
   v-row(align="center")
-    v-col(sm='2' v-if='!isMobile')
+    v-col(sm="2", v-if="!isMobile")
       v-img.triangle-img(src="../assets/pt6_1.png")
-    v-col(style='text-align: center;' cols='12' sm='8')
+    v-col(style="text-align: center", cols="12", sm="8")
       h1 fale conosco!
       p Tem alguma dúvida? Gostaria de nos patrocinar? Deixe aqui suas informações para entrarmos em contato assim que possível!
       v-container
         v-row
-          v-col(cols='12' sm='4')
-            v-text-field.blue-border(v-model="form.name" label="Nome" solo flat hide-details color="#6FDDD5")
-          v-col(cols='12' sm='4')
-            v-text-field.blue-border(v-model="form.email" label="Email" solo flat hide-details color="#6FDDD5")
-          v-col(cols='12' sm='4')
-            v-text-field.blue-border(v-model="form.phone" label="Telefone (opcional)" solo flat hide-details color="#6FDDD5")
-          v-col(cols='12')
-            v-textarea.blue-border(v-model="form.description" label="Digite sua mensagem aqui..." solo flat auto-grow rows="4")
-        v-row.pink-btn(justify='center')
-          v-btn(depressed color="#FF99CC") Enviar
-    v-col(sm='2' v-if='!isMobile')
+          v-col(cols="12", sm="4")
+            v-text-field.blue-border(
+              v-model="form.name",
+              label="Nome",
+              solo,
+              flat,
+              hide-details,
+              color="#6FDDD5"
+            )
+          v-col(cols="12", sm="4")
+            v-text-field.blue-border(
+              v-model="form.email",
+              label="Email",
+              solo,
+              flat,
+              hide-details,
+              color="#6FDDD5"
+            )
+          v-col(cols="12", sm="4")
+            v-text-field.blue-border(
+              v-model="form.phone",
+              label="Telefone (opcional)",
+              solo,
+              flat,
+              hide-details,
+              color="#6FDDD5"
+            )
+          v-col(cols="12")
+            v-textarea.blue-border(
+              v-model="form.message",
+              label="Digite sua mensagem aqui...",
+              solo,
+              flat,
+              auto-grow,
+              rows="4"
+            )
+        v-row.pink-btn(justify="center")
+          v-btn(depressed, color="#FF99CC", @click="sendEmail()") Enviar
+    v-col(sm="2", v-if="!isMobile")
       v-img.wave-img(src="../assets/pt6_2.png")
 </template>
 
 <script>
-  export default {
-    name: 'ContactUs',
+import axios from "axios";
 
-    data: () => ({
-      form:{
-        name: '',
-        email: '',
-        phone: '',
-        description:''
-      }
-    }),
-    computed: {
-      isMobile() {
-        return this.$vuetify.breakpoint.xsOnly
-      }
-    }
-  }
+export default {
+  name: "ContactUs",
+  methods: {
+    sendEmail() {
+      axios
+        .post(
+          "https://gefmeauo7d.execute-api.us-east-2.amazonaws.com/prod/contact/",
+          this.form
+        )
+        .then(
+          function(response) {
+            console.log(response.data);
+          }
+        );
+    },
+  },
+  data: () => ({
+    form: {
+      name: "",
+      email: "",
+      phone: "",
+      message: "",
+    },
+  }),
+  computed: {
+    isMobile() {
+      return this.$vuetify.breakpoint.xsOnly;
+    },
+  },
+};
 </script>
 <style lang="sass" scoped>
 #contact-us
